d72361cccd9865bffa611c462204351104ba93f3
air-kiss <30412695+air-kiss@users.noreply.github.com>
2017-07-24 20:21:25 +0300

    Video fix

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-(function() {
-  /**
-   * Video element
-   * @type {HTMLElement}
-   */
-  var video = document.getElementById("my-video");
-
-  /**
-   * Check if video can play, and play it
-   */
-  video.addEventListener( "canplay", function() {
-    video.play();
-  });
-})();
+// (function() {
+//   /**
+//    * Video element
+//    * @type {HTMLElement}
+//    */
+//   var video = document.getElementById("my-video");
+//
+//   /**
+//    * Check if video can play, and play it
+//    */
+//   video.addEventListener( "canplay", function() {
+//     video.play();
+//   });
+// })();
 
 
 // Find all YouTube videos

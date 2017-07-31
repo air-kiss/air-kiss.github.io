92720a2647b7e8a23dcb0f867bf3c15ccfbebce5
air-kiss <30412695+air-kiss@users.noreply.github.com>
2017-08-01 00:21:50 +0300

    Mute fixed

--- js/main.js
+++ js/main.js
@@ -68,25 +68,25 @@ $(window).load(function() {
   $('.logo').css("top", half);
 
   // Buttons
-  var playButton = document.getElementById("play-pause");
+  // var playButton = document.getElementById("play-pause");
   var muteButton = document.getElementById("mute");
 
   // Event listener for the play/pause button
-  playButton.addEventListener("click", function() {
-    if (video.paused == true) {
-      // Play the video
-      video.play();
-
-      // Update the button text to 'Pause'
-      playButton.innerHTML = "Pause";
-    } else {
-      // Pause the video
-      video.pause();
-
-      // Update the button text to 'Play'
-      playButton.innerHTML = "Play";
-    }
-  });
+  // playButton.addEventListener("click", function() {
+  //   if (video.paused == true) {
+  //     // Play the video
+  //     video.play();
+  //
+  //     // Update the button text to 'Pause'
+  //     playButton.innerHTML = "Pause";
+  //   } else {
+  //     // Pause the video
+  //     video.pause();
+  //
+  //     // Update the button text to 'Play'
+  //     playButton.innerHTML = "Play";
+  //   }
+  // });
 
   // Event listener for the mute button
   muteButton.addEventListener("click", function() {
